fce032e6155850dad75315c896a427b7caff1562
Omar Gonzalez <ogonzalez2212@gmail.com>
2019-08-10 23:51:12 -0500

    Snake Game

---
+++ Assets/Scripts/RandomStartPosition.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomStartPosition : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.changePosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void changePosition()
+    {
+        Vector2 position = new Vector2(Random.Range(-9, 9), Random.Range(-4, 4));
+        while(this.verifyPosition(position))
+        {
+            position = new Vector2(Random.Range(-9, 9), Random.Range(-4, 4));
+        }
+        transform.position = position;
+    }
+
+    bool verifyPosition(Vector2 position)
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        bool flag = false;
+        foreach (GameObject player in players)
+        {
+            if (player.transform.position.x == position.x && player.transform.position.y == position.y)
+            {
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.name == "Snake")
+        {
+            this.changePosition();
+        }
+    }
+}
---
+++ Assets/Scripts/SnakeMovement.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.SceneManagement;
+
+public class SnakeMovement : MonoBehaviour
+{
+    public GameObject TailPrefab;
+    private Vector2 previousPosition;
+    private Vector2 direction = Vector2.right;
+    private List<Transform> tails = new List<Transform>();
+    public float speed = 1.0f;
+
+    private bool flagUpdated = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (flagUpdated)
+        {
+            InvokeRepeating("Move", speed, speed);
+            flagUpdated = false;
+        }
+        if (Input.GetAxis("Horizontal") < 0) {
+            if (tails.Count == 0 || (tails.Count > 0 && direction != Vector2.right))
+            {
+                direction = Vector2.left;
+            }
+        } else if (Input.GetAxis("Horizontal") > 0) {
+            if (tails.Count == 0 || (tails.Count > 0 && direction != Vector2.left))
+            {
+                direction = Vector2.right;
+            }
+        } else if (Input.GetAxis("Vertical") < 0) {
+            if (tails.Count == 0 || (tails.Count > 0 && direction != Vector2.up))
+            {
+                direction = Vector2.down;
+            }
+        } else if (Input.GetAxis("Vertical") > 0) {
+            if (tails.Count == 0 || (tails.Count > 0 && direction != Vector2.down))
+            {
+                direction = Vector2.up;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if(col.name == "Point") {
+            this.grow();
+        } else {
+            bool option = EditorUtility.DisplayDialog("Game Over",
+            "Start again",
+            "Ok",
+            "Close");
+            if (option) {
+                SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+            }
+        }
+    }
+
+    void grow()
+    {
+        GameObject g = (GameObject)Instantiate(TailPrefab, previousPosition, Quaternion.identity);
+        tails.Insert(0, g.transform);
+
+        if (tails.Count % 2 == 0)
+        {
+            if (speed > 0.1f)
+            {
+                speed -= 0.1f;
+                flagUpdated = true;
+                CancelInvoke();
+            }
+        }
+    }
+
+    void Move()
+    {
+        previousPosition = transform.position;
+        transform.Translate(direction);
+        
+        if (tails.Count > 0) {
+            tails[tails.Count-1].position = previousPosition;
+            tails.Insert(0, tails[tails.Count-1]);
+            tails.RemoveAt(tails.Count-1);
+        }
+    }
+}
